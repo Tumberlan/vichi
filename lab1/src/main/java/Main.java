6788b6718d64a3a5f2ee9761b0ac1d52be3a81ed
i.ashikhmin <i.ashikhmin@g.nsu.ru>
2021-09-10 15:23:08 +0700

    10.09.2021 15:23

--- lab1/src/main/java/Main.java
+++ lab1/src/main/java/Main.java
@@ -1,6 +1,13 @@
+import java.io.InputStream;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] arags){
         LInear lInear = new LInear();
-        lInear.allOperations(1,1,1);
+        Scanner input = new Scanner(System.in);
+        String command = input.nextLine();
+        String[] sub_str = command.split(" ");
+        lInear.allOperations(Integer.parseInt(sub_str[0]),Integer.parseInt(sub_str[1]),
+                Integer.parseInt(sub_str[2]));
     }
 }
